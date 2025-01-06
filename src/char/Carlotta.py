7ffc96c002bdf4ed74f5b1896d8cf7cda6beedc2
Toshiko Li <jl1355@duke.edu>
2025-01-06 18:26:19 -0500

    Revise: Carlotta.py

--- src/char/Carlotta.py
+++ src/char/Carlotta.py
@@ -10,5 +10,26 @@ class Carlotta(BaseChar):
             self.logger.debug('has_intro wait click 1.2 sec')
             self.bullet = 1
             self.continues_normal_attack(1.2)
+        if self.is_forte_full():
+            self.heavy_attack()
+            return self.switch_next_char()
+        if self.liberation_available():
+            while self.liberation_available():
+                self.click_liberation()
+                self.check_combat()
+            if self.resonance_available:
+                if self.click_resonance()[0]:
+                    self.click_echo()
+                    return self.switch_next_char()
+            return self.switch_next_char()
+        if self.resonance_available():
+            if self.bullet == 0:
+                self.heavy_attack()
+            if self.click_resonance()[0]:
+                self.click_echo()
+                return self.switch_next_char()
+        if self.echo_available():
+            self.click_echo()
+            return self.switch_next_char()
         self.continues_normal_attack(0.31)
         self.switch_next_char()
